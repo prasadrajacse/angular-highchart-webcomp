12723d285adb556bc05e903200a0820efe5ff960
Durga Prasad <prasadrajacse@gmail.com>
2021-03-08 22:33:54 +0800

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { Injector, NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+
+//import { AppComponent } from './app.component';
+import { OutputGraphComponent } from './output-graph/output-graph.component';
+
+import { createCustomElement } from '@angular/elements';
 
 @NgModule({
   declarations: [
-    AppComponent
+    //AppComponent,
+    OutputGraphComponent
   ],
   imports: [
     BrowserModule
   ],
+  entryComponents: [OutputGraphComponent],
   providers: [],
-  bootstrap: [AppComponent]
+  //bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private injector: Injector){
+    const customNgHighchart = createCustomElement(OutputGraphComponent,{injector});
+    customElements.define('app-output-graph',customNgHighchart);
+  }
+ 
+  ngDoBootstrap(){
+    
+  }
+
+ }
